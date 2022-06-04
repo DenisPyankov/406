ada1929fd747ae494acd89e83064931bf2d5cc3e
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-06-04 08:06:45 +0500

    Update 510.cpp

--- 510.cpp
+++ 510.cpp
@@ -1,1 +1,24 @@
 
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	  ifstream in("in.txt");
+	  ofstream out("out.txt");
+    int N;
+    in >> N;
+    vector <int> v(1 + N,1);
+    for (int i = 2; i <= N; i += 2)
+    {
+        v[i] = v[i - 2] * 3;
+        for (int j = i - 4; j >= 0; j -= 2)
+            v[i] += v[j] * 2;
+    }
+    out << v[N];
+}
