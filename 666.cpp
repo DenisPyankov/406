7c9c08ea95fdbae7a2ed461d918b052d22c5b078
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-06-04 07:59:34 +0500

    Update 666.cpp

--- 666.cpp
+++ 666.cpp
@@ -1,1 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
 
+int main()
+{
+	  ifstream in("in.txt");
+	  ofstream out("out.txt");
+    int N;
+    in >> N;
+    for (int i = 26; i >= 1; i--)
+        if (N != 1) {
+            if (N <= pow(2, i - 1))
+                N--;
+            else
+                N -= pow(2, i - 1);
+        }
+        else {
+            out << char('a' + i - 1);
+            return 0;
+        }
+}
